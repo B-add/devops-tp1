f295d2c89cf3da28cb3c739b4bcbab4b893466b9
Valentin Lamatte <valentin.lamatte@epita.fr>
2017-07-03 17:17:00 +0200

    Add new components

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,19 @@ import Head from 'next/head'
 
 export default () => (
     <Head>
-        {/**TODO**/}
+      <title>Estimeo Highlights</title>
+      <meta name="viewport" content="initial-scale=1.0,
+      width=device-width" />
+      <link href="https://fonts.googleapis.com/css?family=Roboto"
+      rel="stylesheet" />
+      <link rel="stylesheet" href="/static/css/estimeo-style.css" />
+      <style>
+        {`
+          body {
+            font-family: "Reboto";
+            color: #151515;
+          }
+        `}
+      </style>
     </Head>
 )
